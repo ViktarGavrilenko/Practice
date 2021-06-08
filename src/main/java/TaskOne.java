bed6ebf281a35a004aec3ce8c362b000aa100aee
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-06-08 13:10:01 +0300

    Создал класс TaskOne для первого задания

---
+++ src/main/java/TaskOne.java
@@ -0,0 +1,17 @@
+/**
+ * Вводится строка слов. Вывести слова в обратном порядке
+ */
+
+public class TaskOne {
+
+    public static void main(String[] args) {
+
+        String stringWordsIn = "Один два три четыре пять шесть";
+        ReverseWords r = new ReverseWords();
+
+        System.out.println("Введенная строка:" + stringWordsIn);
+        System.out.println("Строка на оборот:" + r.ReverseWordsInString(stringWordsIn));
+
+    }
+
+}
