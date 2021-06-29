2cf363f4741d26d9791b87de205af30db1a19ee4
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-06-29 16:47:42 +0300

    Удалил класс

--- src/main/java/com/example/exception/MyException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exception;
-
-public class MyException extends Exception {
-    public String toString() {
-        return "Ошибка обработана классом " + super.toString();
-    }
-}
