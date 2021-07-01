fb894adeaa504e16dcde8f9501e51a704c030fed
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-01 16:57:02 +0300

    исправил места переменных ассертах

--- src/test/java/com/example/utilities/ArrayUtilitiesTest.java
+++ src/test/java/com/example/utilities/ArrayUtilitiesTest.java
@@ -18,177 +18,177 @@ public class ArrayUtilitiesTest {
     // Тесты для метода getAverage
     @Test(description = "Тестируем получение целого среднеарифметического числа", groups = {"getAverage"})
     public void testGetAverageArrayResultInt() {
-        Assert.assertEquals(3, ArrayUtilities.getAverage(REPEATED_VALUES));
+        Assert.assertEquals(ArrayUtilities.getAverage(REPEATED_VALUES), 3);
     }
 
     @Test(description = "Тестируем пустой массив", groups = {"getAverage"})
     public void testGetAverageArrayEmpty() {
-        Assert.assertEquals(0, ArrayUtilities.getAverage(EMPTY_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getAverage(EMPTY_ARRAY), 0);
     }
 
     @Test(description = "Тестируем получение десятичного среднеарифметического числа", groups = {"getAverage"})
     public void testGetAverageArrayResultFloat() {
-        Assert.assertEquals(6.75, ArrayUtilities.getAverage(POSITIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getAverage(POSITIVE_ARRAY), 6.75);
     }
 
     @Test(description = "Тестируем не инициализированный массив", groups = {"getAverage"})
     public void testGetAverageArrayNull() {
-        Assert.assertEquals(0, ArrayUtilities.getAverage(NULL_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getAverage(NULL_ARRAY), 0);
     }
 
     @Test(description = "Тестируем отрицательные значения в массиве", groups = {"getAverage"})
     public void testGetAverageArrayResultNegative() {
-        Assert.assertEquals(-13, ArrayUtilities.getAverage(NEGATIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getAverage(NEGATIVE_ARRAY), -13);
     }
 
     // Тесты для метода getMaxValue
     @Test(description = "Тестируем положительные значения в массиве", groups = {"getMaxValue"})
     public void testGetMaxValuePositive() {
-        Assert.assertEquals(10, ArrayUtilities.getMaxValue(POSITIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMaxValue(POSITIVE_ARRAY), 10);
     }
 
     @Test(description = "Тестируем отрицательные значения в массиве", groups = {"getMaxValue"})
     public void testGetMaxValueNegative() {
-        Assert.assertEquals(-1, ArrayUtilities.getMaxValue(NEGATIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMaxValue(NEGATIVE_ARRAY), -1);
     }
 
     @Test(description = "Тестируем положительные и отрицательные значения в массиве", groups = {"getMaxValue"})
     public void testGetMaxValueNegativePositive() {
-        Assert.assertEquals(7, ArrayUtilities.getMaxValue(NEGATIVE_POSITIVE));
+        Assert.assertEquals(ArrayUtilities.getMaxValue(NEGATIVE_POSITIVE), 7);
     }
 
     @Test(description = "Тестируем не инициализированный массив", groups = {"getMaxValue"})
     public void testGetMaxValueNull() {
-        Assert.assertEquals(0, ArrayUtilities.getMaxValue(NULL_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMaxValue(NULL_ARRAY), 0);
     }
 
     @Test(description = "Тестируем пустой массив", groups = {"getMaxValue"})
     public void testGetMaxValueEmpty() {
-        Assert.assertEquals(0, ArrayUtilities.getMaxValue(EMPTY_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMaxValue(EMPTY_ARRAY), 0);
     }
 
     // Тесты для метода getMinValue
     @Test(description = "Тестируем положительные значения в массиве", groups = {"getMinValue"})
     public void testGetMinValuePositive() {
-        Assert.assertEquals(3, ArrayUtilities.getMinValue(POSITIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMinValue(POSITIVE_ARRAY), 3);
     }
 
     @Test(description = "Тестируем отрицательные значения в массиве", groups = {"getMinValue"})
     public void testGetMinValueNegative() {
-        Assert.assertEquals(-35, ArrayUtilities.getMinValue(NEGATIVE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMinValue(NEGATIVE_ARRAY), -35);
     }
 
     @Test(description = "Тестируем положительные и отрицательные значения в массиве", groups = {"getMinValue"})
     public void testGetMinValueNegativePositive() {
-        Assert.assertEquals(-35, ArrayUtilities.getMinValue(NEGATIVE_POSITIVE));
+        Assert.assertEquals(ArrayUtilities.getMinValue(NEGATIVE_POSITIVE), -35);
     }
 
     @Test(description = "Тестируем не инициализированный массив", groups = {"getMinValue"})
     public void testGetMinValueNull() {
-        Assert.assertEquals(0, ArrayUtilities.getMinValue(NULL_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMinValue(NULL_ARRAY), 0);
     }
 
     @Test(description = "Тестируем пустой массив", groups = {"getMinValue"})
     public void testGetMinValueEmpty() {
-        Assert.assertEquals(0, ArrayUtilities.getMinValue(EMPTY_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMinValue(EMPTY_ARRAY), 0);
     }
 
     @Test(description = "Тестируем не заполненный массив", groups = {"getMinValue"})
     public void testGetMinValueNotValue() {
-        Assert.assertEquals(0, ArrayUtilities.getMinValue(NOT_VALUE_ARRAY));
+        Assert.assertEquals(ArrayUtilities.getMinValue(NOT_VALUE_ARRAY), 0);
     }
 
     // Тесты для метода getFirstIndexWithGivenValue
     @Test(description = "Тестируем с одним совпадением в массиве", groups = {"getFirstIndexWithGivenValue"})
     public void testGetFirstIndexWithGivenValuePositiveNegative() {
-        Assert.assertEquals(2, ArrayUtilities.getFirstIndexWithGivenValue(NEGATIVE_POSITIVE, -3));
+        Assert.assertEquals(ArrayUtilities.getFirstIndexWithGivenValue(NEGATIVE_POSITIVE, -3), 2);
     }
 
     @Test(description = "Тестируем не заполненный массив", groups = {"getFirstIndexWithGivenValue"})
     public void testGetFirstIndexWithGivenValueNegative() {
-        Assert.assertEquals(-1, ArrayUtilities.getFirstIndexWithGivenValue(NOT_VALUE_ARRAY, -6));
+        Assert.assertEquals(ArrayUtilities.getFirstIndexWithGivenValue(NOT_VALUE_ARRAY, -6), -1);
     }
 
     @Test(description = "Тестируем массив без совпадений", groups = {"getFirstIndexWithGivenValue"})
     public void testGetFirstIndexWithGivenValueNotFound() {
-        Assert.assertEquals(-1, ArrayUtilities.getFirstIndexWithGivenValue(POSITIVE_ARRAY, 12));
+        Assert.assertEquals(ArrayUtilities.getFirstIndexWithGivenValue(POSITIVE_ARRAY, 12), -1);
     }
 
     @Test(description = "Тестируем массив с тремя совпадениями", groups = {"getFirstIndexWithGivenValue"})
     public void testGetFirstIndexWithGivenValueRepeatValue() {
-        Assert.assertEquals(1, ArrayUtilities.getFirstIndexWithGivenValue(REPEATED_VALUES, 4));
+        Assert.assertEquals(ArrayUtilities.getFirstIndexWithGivenValue(REPEATED_VALUES, 4), 1);
     }
 
     @Test(description = "Тестируем не инициализированный массив массив", groups = {"getFirstIndexWithGivenValue"})
     public void testGetFirstIndexWithGivenValueNull() {
-        Assert.assertEquals(-1, ArrayUtilities.getFirstIndexWithGivenValue(NULL_ARRAY, 4));
+        Assert.assertEquals(ArrayUtilities.getFirstIndexWithGivenValue(NULL_ARRAY, 4), -1);
     }
 
     // Тесты для метода getLastIndexWithGivenValue
     @Test(description = "Тестируем с одним совпадением в массиве", groups = {"getLastIndexWithGivenValue"})
     public void testGetLastIndexWithGivenValuePositiveNegative() {
-        Assert.assertEquals(2, ArrayUtilities.getLastIndexWithGivenValue(NEGATIVE_POSITIVE, -3));
+        Assert.assertEquals(ArrayUtilities.getLastIndexWithGivenValue(NEGATIVE_POSITIVE, -3), 2);
     }
 
     @Test(description = "Тестируем не заполненный массив", groups = {"getLastIndexWithGivenValue"})
     public void testGetLastIndexWithGivenValueNegative() {
-        Assert.assertEquals(-1, ArrayUtilities.getLastIndexWithGivenValue(NOT_VALUE_ARRAY, 5));
+        Assert.assertEquals(ArrayUtilities.getLastIndexWithGivenValue(NOT_VALUE_ARRAY, 5), -1);
     }
 
     @Test(description = "Тестируем массив без совпадений", groups = {"getLastIndexWithGivenValue"})
     public void testGetLastIndexWithGivenValueNotFound() {
-        Assert.assertEquals(-1, ArrayUtilities.getLastIndexWithGivenValue(POSITIVE_ARRAY, 12));
+        Assert.assertEquals(ArrayUtilities.getLastIndexWithGivenValue(POSITIVE_ARRAY, 12), -1);
     }
 
     @Test(description = "Тестируем массив с тремя совпадениями", groups = {"getLastIndexWithGivenValue"})
     public void testGetLastIndexWithGivenValueRepeatValue() {
-        Assert.assertEquals(3, ArrayUtilities.getLastIndexWithGivenValue(REPEATED_VALUES, 4));
+        Assert.assertEquals(ArrayUtilities.getLastIndexWithGivenValue(REPEATED_VALUES, 4), 3);
     }
 
     @Test(description = "Тестируем не инициализированный массив", groups = {"getLastIndexWithGivenValue"})
     public void testGetLastIndexWithGivenValueNull() {
-        Assert.assertEquals(-1, ArrayUtilities.getLastIndexWithGivenValue(NULL_ARRAY, 4));
+        Assert.assertEquals(ArrayUtilities.getLastIndexWithGivenValue(NULL_ARRAY, 4), -1);
     }
 
     // Тесты для метода getSumBetweenElements
     @Test(description = "Тестируем массив с первым минимальным и последним максимальным значениями",
             groups = {"getSumBetweenElements"})
     public void testGetSumBetweenElementsFirstMinLastMax() {
-        Assert.assertEquals(-3, ArrayUtilities.getSumBetweenElements(NEGATIVE_POSITIVE, 1,
-                3));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(NEGATIVE_POSITIVE, 1,
+                3), -3);
     }
 
     @Test(description = "Тестируем массив с первым максимальным и последним минимальным значениями",
             groups = {"getSumBetweenElements"})
     public void testGetSumBetweenElementsFirstMaxLastMin() {
-        Assert.assertEquals(3, ArrayUtilities.getSumBetweenElements(NEGATIVE_POSITIVE, 4,
-                2));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(NEGATIVE_POSITIVE, 4,
+                2), 3);
     }
 
     @Test(description = "Тестируем массив с первым элементов выходящим за пределы массива",
             groups = {"getSumBetweenElements"})
     public void testGetSumBetweenElementsFirstOutside() {
-        Assert.assertEquals(0, ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, -3,
-                2));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, -3,
+                2), 0);
     }
 
     @Test(description = "Тестируем массив с последним элементов выходящим за пределы массива",
             groups = {"getSumBetweenElements"})
     public void testGetSumElementsArrayLastOutside() {
-        Assert.assertEquals(0, ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, 2,
-                22));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, 2,
+                22), 0);
     }
 
     @Test(description = "Тестируем массив с одинаковыми первым и последним элементами",
             groups = {"getSumBetweenElements"})
     public void testGetSumBetweenElementsFirstEquallyLast() {
-        Assert.assertEquals(0, ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, 2,
-                2));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(POSITIVE_ARRAY, 2,
+                2), 0);
     }
 
     @Test(description = "Тестируем не инициализированный массив", groups = {"getSumBetweenElements"})
     public void testGetSumBetweenElementsNull() {
-        Assert.assertEquals(0, ArrayUtilities.getSumBetweenElements(NULL_ARRAY, 2, 3));
+        Assert.assertEquals(ArrayUtilities.getSumBetweenElements(NULL_ARRAY, 2, 3), 0);
     }
 
     // Тесты для метода isNullOrEmpty
@@ -284,6 +284,4 @@ public class ArrayUtilitiesTest {
     public void testIsArrayEmptyTextWithSpaces() throws ArrayEmptyException, ArrayNullException {
         Assert.assertTrue(ArrayUtilities.isArrayEmpty(NOT_VALUE_ARRAY));
     }
-
-
 }
