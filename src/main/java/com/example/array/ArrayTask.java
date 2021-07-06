a3dd681758cf3b09ca46f8779a6660b34be147e9
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-06 12:15:04 +0300

    Добавил операторы throws в объявлении методов с исключениями, которые в методах могут возникнуть. Изменил вызов статических методов из класса ArrayUtilities

--- src/main/java/com/example/array/ArrayTask.java
+++ src/main/java/com/example/array/ArrayTask.java
@@ -1,17 +1,18 @@
 package com.example.array;
 
-import com.example.utilities.ArrayUtilities;
+import com.example.exception.ArrayEmptyException;
+import com.example.exception.ArrayNullException;
+import com.example.exception.ValueOutsideArrayException;
 
 import java.util.ArrayList;
 
 import static com.example.utilities.ArrayUtilities.*;
 
 public class ArrayTask {
-    ArrayUtilities arrayUtilities = new ArrayUtilities();
 
     //  Меняем элементы массива в обратном порядке
-    public int[] reverseArray(int[] inArray) {
-        if (arrayUtilities.isNullOrEmpty(inArray)) {
+    public int[] reverseArray(int[] inArray) throws ArrayEmptyException, ArrayNullException {
+        if (isNullOrEmpty(inArray)) {
             int tempVariable;
             int lastValue = inArray.length - 1;
 
@@ -27,8 +28,8 @@ public class ArrayTask {
     }
 
     // Заполняем массив случайными числами от 0 до 5
-    public int[] addRandomValue(int[] inArray) {
-        if (arrayUtilities.isNullOrEmpty(inArray)) {
+    public int[] addRandomValue(int[] inArray) throws ArrayEmptyException, ArrayNullException {
+        if (isNullOrEmpty(inArray)) {
             for (int x = 0; x < inArray.length; x++) {
                 inArray[x] = (int) (Math.random() * 5);
             }
@@ -38,11 +39,11 @@ public class ArrayTask {
     }
 
     // Сравниваем средние арифметические значения двух массивов
-    public byte compareAverageArrays(int[] firstArray, int[] secondArray) {
+    public byte compareAverageArrays(int[] firstArray, int[] secondArray) throws ArrayEmptyException, ArrayNullException {
         float resultCompare;
         byte result = -1;
-        if (arrayUtilities.isNullOrEmpty(firstArray) && arrayUtilities.isNullOrEmpty(secondArray)) {
-            resultCompare = Float.compare(arrayUtilities.getAverage(firstArray), arrayUtilities.getAverage(secondArray));
+        if (isNullOrEmpty(firstArray) && isNullOrEmpty(secondArray)) {
+            resultCompare = Float.compare(getAverage(firstArray), getAverage(secondArray));
             if (resultCompare == 0) {
                 result = 0;
             } else if (resultCompare > 0) {
@@ -55,23 +56,23 @@ public class ArrayTask {
     }
 
     // Поиск максимального и минимального значения каждой строки двумерного массива
-    public int[][] searchMaxMinValueTwoArray(int[][] inArray) {
+    public int[][] searchMaxMinValueTwoArray(int[][] inArray) throws ArrayEmptyException, ArrayNullException {
         int[][] outArray = null;
-        if (arrayUtilities.isNullOrEmpty(inArray)) {
+        if (isNullOrEmpty(inArray)) {
             outArray = new int[inArray.length][2];
             for (int x = 0; x < inArray.length; x++) {
-                outArray[x][0] = arrayUtilities.getMaxValue(inArray[x]);
-                outArray[x][1] = arrayUtilities.getMinValue(inArray[x]);
+                outArray[x][0] = getMaxValue(inArray[x]);
+                outArray[x][1] = getMinValue(inArray[x]);
             }
         }
         return outArray;
     }
 
     // Поиск уникальных значений массива
-    public ArrayList<Integer> searchUniqueValue(int[] inArray) {
+    public ArrayList<Integer> searchUniqueValue(int[] inArray) throws ArrayEmptyException, ArrayNullException {
         ArrayList<Integer> uniqueValue = new ArrayList<>();
 
-        if (arrayUtilities.isNullOrEmpty(inArray)) {
+        if (isNullOrEmpty(inArray)) {
             boolean isUniqueValue = false;
             for (int x = 0; x < inArray.length; x++) {
                 for (int y = 0; y < inArray.length; y++) {
@@ -92,22 +93,21 @@ public class ArrayTask {
     }
 
     // Поиск суммы между максимальным и минимальным элементами массива
-    public int calcSumBetweenMinMax(int[] inArray) {
+    public int calcSumBetweenMinMax(int[] inArray) throws ValueOutsideArrayException, ArrayEmptyException, ArrayNullException {
         int result = 0;
         int indexMax;
         int indexMin;
         int maxValue;
         int minValue;
 
+        if (isNullOrEmpty(inArray)) {
+            maxValue = getMaxValue(inArray);
+            minValue = getMinValue(inArray);
 
-        if (arrayUtilities.isNullOrEmpty(inArray)) {
-            maxValue = arrayUtilities.getMaxValue(inArray);
-            minValue = arrayUtilities.getMinValue(inArray);
+            indexMax = getLastIndexWithGivenValue(inArray, maxValue);
+            indexMin = getFirstIndexWithGivenValue(inArray, minValue);
 
-            indexMax = arrayUtilities.getLastIndexWithGivenValue(inArray, maxValue);
-            indexMin = arrayUtilities.getFirstIndexWithGivenValue(inArray, minValue);
-
-            result = arrayUtilities.getSumBetweenElements(inArray, indexMin, indexMax);
+            result = getSumBetweenElements(inArray, indexMin, indexMax);
         }
 
         return result;
