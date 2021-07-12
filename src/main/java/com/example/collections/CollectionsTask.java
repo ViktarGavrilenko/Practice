e330bd473ee81be992236812cc04962caa6fabbf
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-12 15:40:07 +0300

    Заменил условие сравнения на метод isArrayListNotNull

--- src/main/java/com/example/collections/CollectionsTask.java
+++ src/main/java/com/example/collections/CollectionsTask.java
@@ -1,18 +1,20 @@
 package com.example.collections;
 
 import com.example.exception.ArrayEmptyException;
+import com.example.exception.ArrayListNotNullException;
 import com.example.exception.ArrayNullException;
 
 import java.util.ArrayList;
 
 import static com.example.utilities.ArrayUtilities.getAverage;
+import static com.example.utilities.CollectionsUtilities.isArrayListNotNull;
 
 public class CollectionsTask {
 
     // Удаляет студентов со средним баллом меньше 3
     public ArrayList<Students> dismissStudentGPALessThree(ArrayList<Students> listStudents) throws ArrayEmptyException,
-            ArrayNullException {
-        if (listStudents != null) {
+            ArrayNullException, ArrayListNotNullException {
+        if (isArrayListNotNull(listStudents)) {
             int sizeListStudents = listStudents.size();
 
             for (int x = 0; x < sizeListStudents; x++) {
