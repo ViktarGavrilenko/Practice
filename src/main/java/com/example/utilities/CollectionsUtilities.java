2e8a7d611db121d8bb263c3d4516a409f756ac2f
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-12 15:49:31 +0300

    Создал метод для проверки не инициализированного array листа

--- src/main/java/com/example/utilities/CollectionsUtilities.java
+++ src/main/java/com/example/utilities/CollectionsUtilities.java
@@ -1,5 +1,17 @@
 package com.example.utilities;
 
+import com.example.collections.Students;
+import com.example.exception.ArrayListNotNullException;
+
+import java.util.ArrayList;
+
 public class CollectionsUtilities {
+    // Проверка массива на инициализацию
+    public static boolean isArrayListNotNull(ArrayList<Students> inArrayList) throws ArrayListNotNullException {
+        if (inArrayList == null) {
+            throw new ArrayListNotNullException();
+        }
+        return true;
+    }
 
 }
