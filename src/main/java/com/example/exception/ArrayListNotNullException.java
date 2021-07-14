b0eeeea122163eac1589b6f952980476807b9dcf
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-14 15:10:18 +0300

    Изменил наследование класса от Exception

--- src/main/java/com/example/exception/ArrayListNotNullException.java
+++ src/main/java/com/example/exception/ArrayListNotNullException.java
@@ -1,6 +1,6 @@
 package com.example.exception;
 
-public class ArrayListNotNullException extends Throwable {
+public class ArrayListNotNullException extends Exception {
     public ArrayListNotNullException() {
         super("ArrayList пустой");
     }
