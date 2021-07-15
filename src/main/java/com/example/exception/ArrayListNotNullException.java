c83c35d99b3270359d7f905d7d159cc267b43565
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-15 14:31:59 +0300

    Откорректировал русский текст после смены кодировки

--- src/main/java/com/example/exception/ArrayListNotNullException.java
+++ src/main/java/com/example/exception/ArrayListNotNullException.java
@@ -2,6 +2,6 @@ package com.example.exception;
 
 public class ArrayListNotNullException extends Exception {
     public ArrayListNotNullException() {
-        super("ArrayList пустой");
+        super("ArrayList РїСѓСЃС‚РѕР№");
     }
 }
