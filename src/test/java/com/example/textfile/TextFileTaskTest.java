d717f0a677398498c1398f2f09a33785899a532c
Viktar Gavrilenko <viktargavrilenko@gmail.com>
2021-07-21 12:44:51 +0300

    Откорректировал название класса по всему файлу после изменения

--- src/test/java/com/example/textfile/TextFileTaskTest.java
+++ src/test/java/com/example/textfile/TextFileTaskTest.java
@@ -1,6 +1,6 @@
 package com.example.textfile;
 
-import com.example.utilities.TextFailUtilities;
+import com.example.utilities.TextFileUtilities;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -10,13 +10,13 @@ import java.io.IOException;
 
 public class TextFileTaskTest {
     private TextFileTask textFileTask;
-    private TextFailUtilities textFailUtilities;
+    private TextFileUtilities textFileUtilities;
     private String textOutFile;
 
     @BeforeTest()
     public void setUp() {
         textFileTask = new TextFileTask();
-        textFailUtilities = new TextFailUtilities();
+        textFileUtilities = new TextFileUtilities();
     }
 
     // Тесты для метода readerTextFileAndWriterInfoInFile
@@ -24,14 +24,14 @@ public class TextFileTaskTest {
     public void testReaderTextFileAndWriterInfoInFile() throws IOException {
         textFileTask.readerTextFileAndWriterInfoInFile("textFile.txt");
         textOutFile = "2 сл., 11 сим\n4 сл., 19 сим\n1 сл., 4 сим\n2 сл., 9 сим\n4 стр.";
-        Assert.assertEquals(textFailUtilities.convertTextFileToStr("outTextFile.txt"), textOutFile);
+        Assert.assertEquals(textFileUtilities.getTextFromTextFile("outTextFile.txt"), textOutFile);
     }
 
     @Test(description = "Тестируем подсчет кол-ва строк, слов, символов в пустом файле")
     public void testReaderTextFileAndWriterInfoInFileEmpty() throws IOException {
         textFileTask.readerTextFileAndWriterInfoInFile("textFileEmpty.txt");
         textOutFile = "0 стр.";
-        Assert.assertEquals(textFailUtilities.convertTextFileToStr("outTextFile.txt"), textOutFile);
+        Assert.assertEquals(textFileUtilities.getTextFromTextFile("outTextFile.txt"), textOutFile);
     }
 
     @Test(description = "Тестируем подсчет кол-ва строк, слов, символов в несуществующем файле",
@@ -44,14 +44,14 @@ public class TextFileTaskTest {
     public void testReaderTextFileAndWriterInfoInFileSpaces() throws IOException {
         textFileTask.readerTextFileAndWriterInfoInFile("textFileSpaces.txt");
         textOutFile = "0 сл., 3 сим\n0 сл., 3 сим\n0 сл., 5 сим\n3 стр.";
-        Assert.assertEquals(textFailUtilities.convertTextFileToStr("outTextFile.txt"), textOutFile);
+        Assert.assertEquals(textFileUtilities.getTextFromTextFile("outTextFile.txt"), textOutFile);
     }
 
     @Test(description = "Тестируем подсчет кол-ва строк, слов, символов в файле первая и последняя строка без текста")
     public void testReaderTextFileAndWriterInfoInFileFirstStrEmpty() throws IOException {
         textFileTask.readerTextFileAndWriterInfoInFile("textFileFirstEmpty.txt");
         textOutFile = "0 сл., 0 сим\n2 сл., 7 сим\n2 сл., 8 сим\n0 сл., 0 сим\n1 сл., 8 сим\n0 сл., 0 сим\n6 стр.";
-        Assert.assertEquals(textFailUtilities.convertTextFileToStr("outTextFile.txt"), textOutFile);
+        Assert.assertEquals(textFileUtilities.getTextFromTextFile("outTextFile.txt"), textOutFile);
     }
 
 }
